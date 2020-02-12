c317c759663822dff54aecaff2109aba0d3e39eb
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-02-12 18:09:31 +0200

    Added css,js and fonts bundles

--- aspnet_tutorial/App_Start/BundleConfig.cs
+++ aspnet_tutorial/App_Start/BundleConfig.cs
@@ -26,6 +26,44 @@ namespace aspnet_tutorial
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Assets/css").Include(
+                "~/Assets/css/bootstrap-table.css",
+                "~/Assets/css/bootstrap-theme.css",
+                "~/Assets/css/bootstrap-theme.css.map",
+                "~/Assets/css/bootstrap-theme.min.css",
+                "~/Assets/css/bootstrap.css",
+                "~/Assets/css/bootstrap.css.map",
+                "~/Assets/css/bootstrap.min.css",
+                "~/Assets/css/datepicker.css",
+                "~/Assets/css/datepicker.css",
+                "~/Assets/css/fontawesome.min.css",
+                "~/Assets/css/styles.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Assets/js").Include(
+                "~/Assets/js/bootstrap-datepicker.js",
+                "~/Assets/js/bootstrap-table.js",
+                "~/Assets/js/bootstrap.js",
+                "~/Assets/js/bootstrap-min.js",
+                "~/Assets/js/chart-data.js",
+                "~/Assets/js/chart-min.js",
+                "~/Assets/js/easypiechart-data.js",
+                "~/Assets/js/easypiechart.js",
+                "~/Assets/js/events.js",
+                "~/Assets/js/font-awesome.min.js",
+                "~/Assets/js/html5shiv.min.js",
+                "~/Assets/js/jquery-1.11.1.min.js",
+                "~/Assets/js/lumino.glyphs.js",
+                "~/Assets/js/respond.min.js"
+            ));
+
+            bundles.Add(new Bundle("~/Assets/fonts").Include(
+                "~/Assets/fonts/glyphicons-halfings-regular.eot",
+                "~/Assets/fonts/glyphicons-halfings-regular.svg",
+                "~/Assets/fonts/glyphicons-halfings-regular.ttf",
+                "~/Assets/fonts/glyphicons-halfings-regular.woff"
+            ));
         }
     }
 }
