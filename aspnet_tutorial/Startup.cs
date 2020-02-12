255c5dd8792749b0bfdd9c33ee436e36f1360a25
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-02-12 17:34:40 +0200

    Add project files.

---
+++ aspnet_tutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_tutorial.Startup))]
+namespace aspnet_tutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
