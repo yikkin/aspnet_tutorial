3be5b0346632ce611daf81f96d431c916d634c65
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-08-24 22:27:43 +0200

    Removed first_name and last_name, created "name" property

--- aspnet_tutorial/Models/customer.cs
+++ aspnet_tutorial/Models/customer.cs
@@ -21,8 +21,7 @@ namespace aspnet_tutorial.Models
         }
     
         public int id { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
+        public string name { get; set; }
         public string email { get; set; }
         public string tel { get; set; }
         public string address { get; set; }
