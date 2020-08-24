cf154c8b5753f00c3e8a64c84f224486219a5d39
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-08-24 22:29:00 +0200

    Changed profile_id to id

--- aspnet_tutorial/Controllers/ProfilesController.cs
+++ aspnet_tutorial/Controllers/ProfilesController.cs
@@ -46,7 +46,7 @@ namespace aspnet_tutorial.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "profile_id,profile_name")] profile profile)
+        public ActionResult Create([Bind(Include = "id,profile_name")] profile profile)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace aspnet_tutorial.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "profile_id,profile_name")] profile profile)
+        public ActionResult Edit([Bind(Include = "id,profile_name")] profile profile)
         {
             if (ModelState.IsValid)
             {
