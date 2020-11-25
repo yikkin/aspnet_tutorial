8b88bcfbb1124a4015aaaff3326d6d7c94128c03
Edgar Eldy <ed.eldy21@live.com>
2020-11-25 23:05:07 +0200

    Add a function for getting products by category id using ajax request

--- aspnet_tutorial/Assets/js/main.js
+++ aspnet_tutorial/Assets/js/main.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(function () {
+    $('.id').on('change', function () {
+        var id = $(this).val();
+        //alert(id);
+        $.get('/Orders/getProducts', { id: id }, function (data) {
+            $('#product_id').html(data);
+            });
+    });
+});
