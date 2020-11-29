37fcca4e7abf89c8e1bfb2ea4df06f1019b056e1
Edgar Eldy <ed.eldy21@live.com>
2020-11-29 21:13:10 +0200

    Update main.js : Change id to cat_id. Add option value for retreiving product id and product name data

--- aspnet_tutorial/Assets/js/main.js
+++ aspnet_tutorial/Assets/js/main.js
@@ -1,9 +1,12 @@
 ﻿$(function () {
-    $('.id').on('change', function () {
+    $('#cat_id').on('change', function () {
         var id = $(this).val();
         //alert(id);
         $.get('/Orders/getProducts', { id: id }, function (data) {
-            $('#product_id').html(data);
+            $('#product_id').empty();
+            $.each(data, function (index, row) {
+                $('#product_id').append("<option value='" + row.product_id + "'>" + row.product_name + "</option>")
+            });
             });
     });
 });
